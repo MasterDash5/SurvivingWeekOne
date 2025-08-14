3c116ca955fd1e743ab81fc7feae8c44c9f3f9c5
MasterDash5 <constant4337@molecularmail.com>
2025-08-14 15:28:11 -0600

    Main menu page

--- frontend/app/game/page.tsx
+++ frontend/app/game/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import background from "@/public/background.png"
 
 export default function Home() {
   return (
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,28 +1,17 @@
 import Image from "next/image";
+import background from "@/public/background.png" 
 
 export default function Home() {
   return (
-		<div className="h-screen w-full flex">
-      <div className="w-2xs bg-zinc-900 text-white p-4 justify-between flex flex-col">
-				<div>
-				  <h2 className="text-lg font-bold text-center text-[#fd0]">Surviving Week One</h2>
-					<div className="mt-8">
-						<p>player stats stuff</p>
-					</div>
-				</div>
-				<div className="w-full flex">
-        	<button className="bg-zinc-700 hover:bg-yellow-500 rounded-md m-2 w-full">Quit Game</button>
-					<button className="bg-zinc-700 hover:bg-yellow-500 rounded-md m-2 w-full">Settings</button>
-			  </div>
-			</div>
-
-      <div className="flex-1 bg-black-900 text-white justify-center flex flex-col">
-        <div className="flex flex-col md:flex-row mt-auto">
-          <button>
-						text
-          </button>
-        </div>
-      </div>
-    </div>
+		<div>
+		 	<Image src={background} alt="background" layout="fill" objectFit="cover" quality={100} priority className="-z-10 blur-xs"/>
+		  <div className="w-1/3 bg-zinc-900 text-white m-auto mt-16 rounded-md">
+			  <div className="flex flex-col justify-center">
+					<h1 className="text-4xl font-bold m-10 text-center text-[#fd0]">Surviving Week One</h1>
+				  <button className="bg-zinc-700 hover:bg-yellow-500 rounded-md mb-8 m-auto h-16 w-3/5">Start Game</button>
+				  <button className="bg-zinc-700 hover:bg-yellow-500 rounded-md mb-8 m-auto h-16 w-3/5">Settings</button>
+		 	  </div>
+		  </div>
+		</div>
   );
 }
