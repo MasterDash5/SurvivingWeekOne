af41d46f63b73083ea0d2c21fdd9027a58720032
MasterDash5 <constant4337@molecularmail.com>
2025-08-18 17:41:59 -0600

    Start stats info

--- frontend/app/game/page.tsx
+++ frontend/app/game/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import background from "@/public/background.png"
+import heart from "@/public/heart.png"
 
 export default function Home() {
   return (
@@ -9,8 +10,12 @@ export default function Home() {
         <div className="w-2xs bg-zinc-900 text-white p-4 justify-between flex flex-col">
 				  <div>
 				    <h2 className="text-xl font-bold text-center text-[#fd0]">Surviving Week One</h2>
-					  <div className="mt-8">
-						  <p>player stats stuff</p>
+					  <div className="mt-8 ml-2">
+						  <h4 className="mb-4 text-lg">Player Name</h4>
+							<div className="flex">
+								<Image src={heart} alt="heart" width={16} className="h-7 w-7"/>
+								<p className="ml-2 text-xl">20</p>
+							</div>
 					  </div>
 				  </div>
 				  <div className="w-full flex">
@@ -22,7 +27,7 @@ export default function Home() {
         <div className="flex flex-col w-full h-screen">
 
 	  		  <div className="mr-24 ml-24 mt-16 mb-16 bg-zinc-700 opacity-80 text-lg text-white h-screen">
-					  <p>text box</p>
+					  <p className="text-center align-middle h-full">text box</p>
 				  </div>
 				  <div className="flex mr-24 ml-24 mb-16 text-white">
 					  <button className="bg-zinc-700 hover:bg-yellow-500 rounded-md h-12 m-8 mt-2 mb-2 w-1/3">Option 1</button>
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
 		<div>
 		 	<Image src={background} alt="background" layout="fill" objectFit="cover" quality={100} priority className="-z-10 blur-xs"/>
-		  <div className="w-1/3 bg-zinc-900 text-white m-auto mt-16 rounded-md">
+		  <div className="w-1/3 bg-zinc-900 text-white m-auto mt-24 rounded-md">
 			  <div className="flex flex-col justify-center">
 					<h1 className="text-4xl font-bold m-10 text-center text-[#fd0]">Surviving Week One</h1>
 				  <button className="bg-zinc-700 hover:bg-yellow-500 rounded-md mb-8 m-auto h-16 w-3/5">Start Game</button>
